18718b91550042d1954d17d2ebc8abb89c48cbfd
aoklyunin <aoklyunin@gmail.com>
2017-08-22 12:02:17 +0300

    фы

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,44 +1,14 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashSet;
 
 /**
  * Created by aokly on 17.08.2017.
  */
 public class Price {
-    public Price(long id, String product_code, int number, int depart, Date begin, Date end, long value) {
-        this.id = id;
-        this.product_code = product_code;
-        this.number = number;
-        this.depart = depart;
-        this.begin = begin;
-        this.end = end;
-        this.value = value;
-    }
-
-    private long id; // идентификатор в БД
-    private String product_code; // код товара
-    private int number; // номер цены
-    private int depart; // номер отдела
-    private Date begin; // начало действия
-    private Date end; // конец действия
-    private long value; // значение цены в копейках
-
-    public long getId() {
-        return id;
-    }
-
-    public String getProduct_code() {
-        return product_code;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public int getDepart() {
-        return depart;
-    }
 
     public Date getBegin() {
         return begin;
@@ -54,17 +24,10 @@ public class Price {
 
     // копирование цены с новыми временными границами
     public Price(Price price, Date begin, Date end) {
-        this.id = price.id;
-        this.product_code = price.product_code;
-        this.number = price.number;
-        this.depart = price.depart;
-        this.value = price.value;
-        this.begin = begin;
-        this.end = end;
+        this(price.product_code,price.number,price.depart,begin,end,price.value);
     }
 
-
-
+/*
     @Override
     public String toString() {
         DateFormat df = new SimpleDateFormat("dd.MM");
@@ -77,28 +40,33 @@ public class Price {
                 ", end=" + df.format(end) +
                 ", value=" + value +
                 '}';
-    }
-   /* @Override
-    public String toString() {
-        DateFormat df = new SimpleDateFormat("dd.MM");
-        return  "{begin=" + df.format(begin) +
-                ", end=" + df.format(end) +
-                ", value=" + value +
-                '}';
     }*/
 
-    public boolean claimCrossed(Price price) {
-        return this.product_code == price.product_code && this.number == price.number;
+    public String toString(){
+        DateFormat df = new SimpleDateFormat("dd.MM");
+        return "{"+this.product_code+"|"+this.number+"|"+this.depart+ "[" + df.format(begin) +"-"+ df.format(end)+"] "+this.value+"}";
     }
 
-    Price(Price price) {
-        this.id = price.id;
-        this.product_code = price.product_code;
-        this.number = price.number;
-        this.depart = price.depart;
-        this.begin = price.begin;
-        this.end = price.end;
-        this.value = price.value;
+
+    public boolean claimCrossed(Price price) {
+        return this.product_code == price.product_code && this.number == price.number&&this.depart==price.depart&&
+                !(this.begin.equals(price.begin)&&this.end.equals(price.end));
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public String getProduct_code() {
+        return product_code;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getDepart() {
+        return depart;
     }
 
     @Override
@@ -108,24 +76,63 @@ public class Price {
 
         Price price = (Price) o;
 
-        if (id != price.id) return false;
         if (number != price.number) return false;
         if (depart != price.depart) return false;
-        if (value != price.value) return false;
         if (product_code != null ? !product_code.equals(price.product_code) : price.product_code != null) return false;
         if (begin != null ? !begin.equals(price.begin) : price.begin != null) return false;
         return end != null ? end.equals(price.end) : price.end == null;
     }
 
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (product_code != null ? product_code.hashCode() : 0);
-        result = 31 * result + number;
-        result = 31 * result + depart;
-        result = 31 * result + (begin != null ? begin.hashCode() : 0);
-        result = 31 * result + (end != null ? end.hashCode() : 0);
-        result = 31 * result + (int) (value ^ (value >>> 32));
-        return result;
+    private static int uniqueId = 0;
+
+    public static Comparator<Price> getComparator(){
+        return new Comparator<Price>() {
+            @Override
+            public int compare(Price o1, Price o2) {
+                int dP = o1.getProduct_code().compareTo(o2.getProduct_code());
+                if (dP != 0)
+                    return dP;
+                else {
+                    int dD = o1.getDepart() - o2.getDepart();
+                    if (dD != 0)
+                        return dD;
+                    else {
+                        int dN = o1.getNumber() - o2.getNumber();
+                        if (dN != 0)
+                            return dN;
+                        else {
+                            return o1.getBegin().compareTo(o2.getBegin());
+                        }
+                    }
+                }
+            }
+        };
     }
+
+    private static int getUniqueId(){
+        return uniqueId++;
+    }
+
+    public Price(long id, String product_code, int number, int depart, Date begin, Date end, long value) {
+        this.id = id;
+        this.product_code = product_code;
+        this.number = number;
+        this.depart = depart;
+        this.begin = begin;
+        this.end = end;
+        this.value = value;
+    }
+
+    public Price(String product_code, int number, int depart, Date begin, Date end, long value) {
+        this(Price.getUniqueId(),product_code,number,depart,begin,end,value);
+    }
+
+    private long id; // идентификатор в БД
+    private String product_code; // код товара
+    private int number; // номер цены
+    private int depart; // номер отдела
+    private Date begin; // начало действия
+    private Date end; // конец действия
+    private long value; // значение цены в копейках
+
 }
--- src/main/java/PriceAdmin.java
+++ src/main/java/PriceAdmin.java
@@ -1,118 +1,180 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by aokly on 17.08.2017.
  */
 public class PriceAdmin {
-    public static ReturnPrice cross(Price oldPrice, Price newPrice) throws UnpredictableCaseException {
-        Date bOld = oldPrice.getBegin(); // начало старой цены
-        Date bNew = newPrice.getBegin(); // начало новой цены
-        Date eOld = oldPrice.getEnd(); // конец старой цены
-        Date eNew = newPrice.getEnd(); // конец старой цены
-        if (bOld.equals(bNew) && eOld.equals(eNew))  // если периоды совпадают
-            return new ReturnPrice(newPrice);
-        else if (bOld.after(eNew) || eOld.before(bNew)) // если периоды вообще не пересекаются
-            return new ReturnPrice(null, oldPrice, newPrice);
-            // случаи пересечения
-        else {
-            if (bOld.before(bNew) && eOld.before(eNew))
-                return new ReturnPrice(
-                        new Price(newPrice, bNew, eOld),
-                        new Price(oldPrice, bOld, bNew),
-                        new Price(newPrice, eOld, eNew));
-            if (bOld.before(bNew) && eOld.after(eNew))
-                return new ReturnPrice(
-                        newPrice,
-                        new HashSet<Price>(Arrays.asList(new Price(oldPrice, bOld, bNew),
-                                new Price(oldPrice, eNew, eOld))),
-                        new HashSet<Price>());
-            if (bOld.before(bNew)&&eOld.equals(eNew))
-                return  new ReturnPrice(
-                        newPrice,
-                        new HashSet<Price>(Collections.singletonList(new Price(oldPrice,bOld,bNew))),
-                        new HashSet<Price>());
-            if (bOld.after(bNew)&&eOld.after(eNew))
-                return new ReturnPrice(
-                        new Price(newPrice,bOld,eNew),
-                        new Price(oldPrice,bNew,eOld),
-                        new Price(newPrice,eNew,bOld)
-                );
-            if (bOld.equals(bNew)&&eNew.before(eOld))
-                return new ReturnPrice(
-                        newPrice,
-                        new Price(oldPrice,eNew,eOld),
-                        new HashSet<Price>()
-                );
-            if (bOld.after(bNew)&&eOld.before(eNew))
-                return new ReturnPrice(
-                        new Price(newPrice,bOld,eOld),
-                        new HashSet<Price>(),
-                        new HashSet<Price>(Arrays.asList(new Price(newPrice,bNew,bOld),new Price(newPrice,eOld,eNew)))
-                );
-        }
-        // выбрасываем исключение о непредсказумом сочетании новой и старой цены
-        throw new UnpredictableCaseException(oldPrice, newPrice);
+
+
+    private static HashSet<Price> splitPrice(Price price, Date date) {
+        //System.out.println(price.getBegin() + ";" + price.getEnd() + ";" + date);
+        // если дата не попадает в период использования цены
+        if (price.getBegin().compareTo(date) * price.getEnd().compareTo(date) > 0 || price.getBegin().equals(date) || price.getEnd().equals(date))
+            return new HashSet<>();
+        else
+            return new HashSet<>(Arrays.asList(
+                    new Price(price, price.getBegin(), date),
+                    new Price(price, date, price.getEnd())
+            ));
     }
 
+    public static void splitOneByOther(Collection<Price> one, Collection<Price> other) {
+        for (Price np : one) {
+            HashSet<Price> tmpPrices = new HashSet<>();
+            HashSet<Price> toDelete = new HashSet<>();
+            for (Price op : other) {
+                //     System.out.println(">"+np+"-"+op);
+                if (np.claimCrossed(op)) {
+                    //       System.out.println("claimed");
+                    HashSet<Price> s2 = new HashSet<>();
+                    HashSet<Price> s1 = splitPrice(op, np.getBegin());
+                    //System.out.println(np+" "+op);
+                    //System.out.println(s1);
+                    if (s1.isEmpty()) {
+                        s2 = splitPrice(op, np.getEnd());
+                    } else {
+                        HashSet<Price> toDeleteTmp = new HashSet<>();
+                        for (Price p : s1) {
+                            HashSet<Price> tmpS = splitPrice(p, np.getEnd());
+                            s2.addAll(tmpS);
+                            if (!tmpS.isEmpty())
+                                toDeleteTmp.add(p);
+                        }
+                        s1.removeAll(toDeleteTmp);
+                    }
+                    System.out.println("s1: " + s1);
+                    System.out.println("s2: " + s2);
+                    System.out.println("tmp before: " + tmpPrices);
+                    tmpPrices.addAll(s1);
+                    tmpPrices.addAll(s2);
+                    System.out.println("tmp: " + tmpPrices);
 
-    public Collection<Price> addNewPrices(Collection<Price> oldPrices, Collection<Price> newPrices) {
-        for (Price price : newPrices) {
-            //fo
-        }
-        return null;
-    }
-
-
-    public static void main(String[] args) {
-        //Co
-    }
-
-    static class UnpredictableCaseException extends Exception {
-        UnpredictableCaseException(Price oldPrice, Price newPrice) {
-            super("oldPrice: " + oldPrice + "\nnewPrice: " + newPrice);
+                    if (!(s1.isEmpty() && s2.isEmpty()))
+                        toDelete.add(op);
+                }
+            }
+            System.out.println("one: " + one);
+            System.out.println("toDelete: " + toDelete);
+            System.out.println("other before delete: " + other);
+            other.removeAll(toDelete);
+            System.out.println("other after delete: " + other);
+            other.addAll(tmpPrices);
+            System.out.println("other after add: " + other);
         }
     }
 
-    // объект, возвращаемый после сравнения цен
-    public static class ReturnPrice {
-        // цена, которую уже заменили, её нужно просто добавить в массив выходных цен
-        Price replacedPrice;
-        // старые цены, которые остались после сравнения
-        HashSet<Price> remainedPrices;
-        // части новой цены, которые не перекрывают старую
-        HashSet<Price> needCheckCross;
+    static void dispPrices(Collection<Price> prices, String caption) {
+        System.out.println("______________________________");
+        System.out.println(caption);
+        for (Price p : prices)
+            System.out.println(p);
+    }
 
-        ReturnPrice(Price replacedPrice, HashSet<Price> remainedPrices, HashSet<Price> needCheckCross) {
-            this.replacedPrice = replacedPrice;
-            this.remainedPrices = new HashSet<Price>(remainedPrices);
-            this.needCheckCross = new HashSet<Price>(needCheckCross);
+    static void dispMap(Map<SubPrice, List<Price>> prices, String caption) {
+        System.out.println("______________________________");
+        System.out.println(caption);
+        for (Map.Entry<SubPrice, List<Price>> p : prices.entrySet()) {
+            System.out.println(p.getKey());
+            for (Price price : p.getValue()) {
+                System.out.println("> " + price);
+            }
+        }
+    }
+
+
+    public static Map<SubPrice, List<Price>> splitPrices(LinkedList<Price> oldPrices, LinkedList<Price> newPrices) {
+        // System.out.println(oldPrices);
+        // System.out.println(newPrices);
+        //System.out.println("__________________");
+        // разбиваем старые цены новыми
+        dispPrices(newPrices, "Новые");
+        dispPrices(oldPrices, "Старые");
+        System.out.println("Разделение");
+        splitOneByOther(newPrices, oldPrices);
+        System.out.println("_____________________________");
+        splitOneByOther(oldPrices, newPrices);
+        Collections.sort(newPrices, Price.getComparator());
+        dispPrices(newPrices, "Новые");
+        dispPrices(oldPrices, "Старые");
+
+
+        // System.out.println("processed");
+        // System.out.println(oldPrices);
+        //   System.out.println(newPrices);
+
+        // в старых ценах оставляем только те, которых нет среди новых
+        // в проверке на равенство значение цены(value) не учитывается,
+        // небольшой чит, иначе пришлось бы писать больший объём кода.
+        oldPrices.removeAll(newPrices);
+
+        dispPrices(oldPrices, "После удаления старые");
+
+
+        // объединяем две коллекции цен в отсортированном порядке
+        HashMap<SubPrice, List<Price>> res = new HashMap<>((Stream.concat(newPrices.stream(), oldPrices.stream())).map(p -> new AbstractMap.SimpleEntry<>(new SubPrice(p), p)).
+                collect(Collectors.groupingBy(Map.Entry::getKey,
+                        Collectors.mapping(Map.Entry::getValue, Collectors.toList()))));
+
+        for (Map.Entry<SubPrice, List<Price>> e : res.entrySet()) {
+            e.getValue().sort(Comparator.comparing(Price::getBegin));
+        }
+        dispMap(res, "словарь");
+        return res;
+    }
+
+    public static Collection<Price> addNewPrices(Collection<Price> _oldPrices, Collection<Price> _newPrices) {
+        // копируем коллекции, чтобы не испортить данные, пришедшие извне
+        LinkedList<Price> oldPrices = new LinkedList<Price>();
+        oldPrices.addAll(_oldPrices);
+        LinkedList<Price> newPrices = new LinkedList<Price>();
+        newPrices.addAll(_newPrices);
+
+        Map<SubPrice, List<Price>> prices = splitPrices(oldPrices, newPrices);
+
+        // схлопываем соседние равные цены
+        LinkedList<Price> res = new LinkedList<>();
+        for (List<Price> ps : prices.values()) {
+            LinkedHashSet<Price> needsToBeDeleted = new LinkedHashSet<>();
+            // Collections.sort(ps, Comparator.comparing(Price::getBegin));
+            for (int i = 0; i < ps.size() - 1; i++) {
+                Price pi = ps.get(i);
+                Price pip1 = ps.get(i + 1);
+                if (pi.getEnd().equals(pip1.getBegin()) && pi.getValue() == pip1.getValue()) {
+                    pip1.setBegin(pi.getBegin());
+                    needsToBeDeleted.add(pi);
+                }
+            }
+            ps.removeAll(needsToBeDeleted);
+            res.addAll(ps);
+        }
+        dispPrices(res, "После объединения");
+        Collections.sort(res,Price.getComparator());
+        return res;
+    }
+
+    public static class SubPrice {
+        private String product_code; // код товара
+        private int number; // номер цены
+        private int depart; // номер отдела
+
+        public SubPrice(String product_code, int number, int depart) {
+            this.product_code = product_code;
+            this.number = number;
+            this.depart = depart;
         }
 
-        ReturnPrice(Price replacedPrice, Price remainedPrice, HashSet<Price> needCheckCross) {
-            this(replacedPrice,
-                    new HashSet<Price>(Collections.singletonList(remainedPrice)),
-                    needCheckCross);
+        public SubPrice(Price p) {
+            this.product_code = p.getProduct_code();
+            this.number = p.getNumber();
+            this.depart = p.getDepart();
         }
 
-        ReturnPrice(Price replacedPrice, HashSet<Price> remainedPrice, Price needCheckCross) {
-            this(replacedPrice,
-                    remainedPrice,
-                    new HashSet<Price>(Collections.singletonList(needCheckCross)));
-        }
-
-
-        ReturnPrice(Price replacedPrice, Price remainedPrice, Price needCheckCross) {
-            this(replacedPrice,
-                    new HashSet<Price>(Collections.singletonList(remainedPrice)),
-                    new HashSet<Price>(Collections.singletonList(needCheckCross)));
-        }
-
-        ReturnPrice(Price replacedPrice) {
-            this(replacedPrice, new HashSet<Price>(), new HashSet<Price>());
-        }
-
-        ReturnPrice() {
-            this(null, new HashSet<Price>(), new HashSet<Price>());
+        public String toString() {
+            return "{" + this.product_code + "|" + this.number + "|" + this.depart + "}";
         }
 
         @Override
@@ -120,30 +182,19 @@ public class PriceAdmin {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
 
-            ReturnPrice that = (ReturnPrice) o;
+            SubPrice subPrice = (SubPrice) o;
 
-            if (replacedPrice != null ? !replacedPrice.equals(that.replacedPrice) : that.replacedPrice != null)
-                return false;
-            if (remainedPrices != null ? !remainedPrices.equals(that.remainedPrices) : that.remainedPrices != null)
-                return false;
-            return needCheckCross != null ? needCheckCross.equals(that.needCheckCross) : that.needCheckCross == null;
+            if (number != subPrice.number) return false;
+            if (depart != subPrice.depart) return false;
+            return product_code != null ? product_code.equals(subPrice.product_code) : subPrice.product_code == null;
         }
 
         @Override
         public int hashCode() {
-            int result = replacedPrice != null ? replacedPrice.hashCode() : 0;
-            result = 31 * result + (remainedPrices != null ? remainedPrices.hashCode() : 0);
-            result = 31 * result + (needCheckCross != null ? needCheckCross.hashCode() : 0);
+            int result = product_code != null ? product_code.hashCode() : 0;
+            result = 31 * result + number;
+            result = 31 * result + depart;
             return result;
-        }
-
-        @Override
-        public String toString() {
-            return "ReturnPrice{" +
-                    "replacedPrice=" + replacedPrice +
-                    ", remainedPrices=" + remainedPrices +
-                    ", needCheckCross=" + needCheckCross +
-                    '}';
         }
     }
 }
